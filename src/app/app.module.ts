d008243b50e6f17f16810dd9110b146e5b711e6a
John Peter <jay3pee@gmail.com>
2021-05-20 05:01:11 +0100

    Added route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
-import { HourlyComponent } from './components/hourly/hourly.component';
-import { DailyComponent } from './components/daily/daily.component';
+import { HourlyComponent } from "./components/hourly/hourly.component";
+import { DailyComponent } from "./components/daily/daily.component";
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: AppComponent,
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HourlyComponent, DailyComponent],
-  imports: [BrowserModule, HttpClientModule, FontAwesomeModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HourlyComponent,
+    DailyComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
